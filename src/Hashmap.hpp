572ac2138854d80d3b70d979c30c51b63b9fdc4e
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2023-12-12 01:32:06 +0800

    Added some more apis

--- src/Hashmap.hpp
+++ src/Hashmap.hpp
@@ -1,19 +1,56 @@
 #pragma once
 
 #include <vector>
+#include <utility>
 
 template<typename KeyType, typename DataType>
 class Hashmap
 {
 public:
+    using MapType = std::pair<KeyType, DataType>;
     Hashmap() {}
     ~Hashmap() {}
+
+    void insert(MapType);
+    void erase();
+    size_t size() const;
+    size_t empty() const;
+
 private:
     struct Entry
     {
+        Entry(KeyType inKey, DataType inData)
+            : key(inKey), data(inData)
+        {}
+
         KeyType key;
         DataType data;
     };
 
     std::vector<Entry> mEntry;
 };
+
+template<typename KeyType, typename DataType>
+void Hashmap<KeyType, DataType>::insert(MapType map)
+{
+    Entry entry{map.first, map.second};
+    mEntry.emplace_back(std::move(entry));
+}
+
+template<typename KeyType, typename DataType>
+void Hashmap<KeyType, DataType>::erase()
+{
+    mEntry.clear();
+}
+
+template<typename KeyType, typename DataType>
+size_t Hashmap<KeyType, DataType>::size() const
+{
+    return mEntry.size();
+}
+
+template<typename KeyType, typename DataType>
+size_t Hashmap<KeyType, DataType>::empty() const
+{
+    return mEntry.empty();
+}
--- tests/HashmapTest.cpp
+++ tests/HashmapTest.cpp
@@ -5,3 +5,21 @@ TEST(HashmapTest, checkEntry)
 {
     EXPECT_NO_THROW((Hashmap<int, int>()));
 }
+
+TEST(HashmapTest, checkInsert)
+{
+    Hashmap<int, int> hashMap;
+    EXPECT_EQ(hashMap.size(), 0);
+    EXPECT_TRUE(hashMap.empty());
+    
+    int insertCount = 10;
+    for(int index = 0; index < insertCount; index++) {
+        EXPECT_NO_THROW(hashMap.insert({index, index * 2}));
+        EXPECT_FALSE(hashMap.empty());
+    }
+    EXPECT_EQ(hashMap.size(), insertCount);
+
+    EXPECT_NO_THROW(hashMap.erase());
+    EXPECT_TRUE(hashMap.empty());
+    EXPECT_EQ(hashMap.size(), 0);
+}
